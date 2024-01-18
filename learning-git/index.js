2db1c1d6b5268ae59659806fa48a96fe7b382d33
yunggoldenson <adesegunlamidi13@gmail.com>
2024-01-18 22:55:18 +0000

    added html code

---
+++ learning-git/index.js
@@ -0,0 +1,15 @@
+MY NAME IS  LAWRENCE 
+GOLDEN
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
